c35e82958285ea57cb4ce946f2fffda1465b43bc
AGK-1 <wiktorasonovic@outlook.com>
2026-04-08 02:47:52 +0400

    student ch

--- main.c
+++ main.c
@@ -1,6 +1,42 @@
 #include <stdio.h>
+#include <time.h>
 
 int main(void) {
-    printf("Hello, World!\n");
+    char ch;
+    char adc;
+    while (1) {
+        // Сначала показываем меню
+        printf("\na for add students\n");
+        printf("d for delete students\n");
+        printf("s for show students\n");
+        printf("u for update students\n");
+        printf("q for quit\n");
+        printf("Select option: ");
+
+        // Потом ждём ввод
+        ch = getchar();
+        if (ch == '\n') continue;
+
+        // Очищаем буфер от Enter
+        int c;
+        while ((c = getchar()) != '\n' && c != EOF);
+
+        if (ch == 'a') {
+            printf("Add user name...\n");
+            adc = getchar();
+        } else if (ch == 'd') {
+            printf("Deleting student...\n");
+        } else if (ch == 's') {
+            printf("Showing students...\n");
+        } else if (ch == 'u') {
+            printf("Updating student...\n");
+        } else if (ch == 'q') {
+            printf("Exit...\n");
+            break;
+        } else {
+            printf("Unknown option: '%c'\n", ch);
+        }
+    }
+
     return 0;
 }
